3234398c12798f073e6b02aa5e5502fb6e1ce0a5
KevinM2468 <kevinm246816@gmail.com>
2023-09-25 00:24:53 -0400

    implemented isValid() function

--- src/objects.ts
+++ src/objects.ts
@@ -42,6 +42,18 @@ export function isCorrect(question: Question, answer: string): boolean {
  * be exactly one of the options.
  */
 export function isValid(question: Question, answer: string): boolean {
+    const { type, options } = question;
+
+    if (type == "short_answer_question") {
+        return true;
+    }
+
+    if (type == "multiple_choice_question") {
+        return options.includes(answer);
+    }
+
+    // The documentation does not include any more examples
+    // I will assume this means there are no other valid cases
     return false;
 }
 
