d24b72923eb28b27b400cead195a363d2c7d69ba
KevinM2468 <kevinm246816@gmail.com>
2023-10-02 09:42:02 -0400

    implemented sumPoints() and sumPublishedPoints() functions

--- src/nested.ts
+++ src/nested.ts
@@ -95,14 +95,21 @@ export function getNames(questions: Question[]): string[] {
  * Consumes an array of questions and returns the sum total of all their points added together.
  */
 export function sumPoints(questions: Question[]): number {
-    return 0;
+    return questions.reduce((sum: number, current: Question): number => {
+        return sum + current.points;
+    }, 0);
 }
 
 /***
  * Consumes an array of questions and returns the sum total of the PUBLISHED questions.
  */
 export function sumPublishedPoints(questions: Question[]): number {
-    return 0;
+    return questions.reduce((sum: number, current: Question): number => {
+        if (current.published) {
+            return sum + current.points;
+        }
+        return sum;
+    }, 0);
 }
 
 /***
