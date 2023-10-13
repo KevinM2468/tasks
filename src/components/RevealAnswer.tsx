550a4abbbe66bd8c538bd0b17131351a335bec87
KevinM2468 <kevinm246816@gmail.com>
2023-10-12 20:06:22 -0400

    implemented RevealAnswer() function

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,16 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [visible, setVisible] = useState<boolean>(false);
+
+    function makeAnswerVisible() {
+        setVisible(true);
+    }
+
+    return (
+        <span>
+            <Button onClick={makeAnswerVisible}>Reveal Answer</Button>
+            {visible && <div>42</div>}
+        </span>
+    );
 }
