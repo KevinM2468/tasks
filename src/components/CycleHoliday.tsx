7844871334b0122969544760907e2707c4b7b096
KevinM2468 <kevinm246816@gmail.com>
2023-10-12 22:21:15 -0400

    Implemented CycleHoliday() function

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,41 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+// New Years Day, Earth Day, Thanksgiving, Christmas, New Years Eve
+type Holiday = "🎊" | "🌎" | "🦃" | "🎄" | "🎉";
+
+const YEAR_TRANSITIONS: Record<Holiday, Holiday> = {
+    "🎊": "🌎",
+    "🌎": "🦃",
+    "🦃": "🎄",
+    "🎄": "🎉",
+    "🎉": "🎊"
+};
+
+const ALPHA_TRANSITIONS: Record<Holiday, Holiday> = {
+    "🎊": "🎉",
+    "🌎": "🎊",
+    "🦃": "🎄",
+    "🎄": "🌎",
+    "🎉": "🦃"
+};
+
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holiday, setHoliday] = useState<Holiday>("🎊");
+
+    function forwardMonth() {
+        setHoliday(YEAR_TRANSITIONS[holiday]);
+    }
+
+    function forwardAlpha() {
+        setHoliday(ALPHA_TRANSITIONS[holiday]);
+    }
+
+    return (
+        <span>
+            <div>Holiday: {holiday}</div>
+            <Button onClick={forwardMonth}>Advance by Year</Button>
+            <Button onClick={forwardAlpha}>Advance by Alphabet</Button>
+        </span>
+    );
 }
