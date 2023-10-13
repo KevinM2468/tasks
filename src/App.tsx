f8c2fecb1ae6ee8e3b74186e1f73051c663b5f0f
KevinM2468 <kevinm246816@gmail.com>
2023-10-12 22:22:18 -0400

    re-added my original main branch App.tsx content

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
 import { StartAttempt } from "./components/StartAttempt";
@@ -13,6 +14,56 @@ function App(): JSX.Element {
             <header className="App-header">
                 UM COS420 with React Hooks and TypeScript
             </header>
+            <div>
+                <h2>This is a thing to introduce web work</h2>
+                <p>
+                    Edit <code>src/App.tsx</code> and save. This page will
+                    automatically reload.
+                </p>
+                <p>Hello World! This is my page.</p>
+                <p>Edited by: Kevin Menenello</p>
+                <div>
+                    <Container>
+                        <Row>
+                            <Col>
+                                <p>
+                                    As of writing this line, I have three things
+                                    left to do
+                                </p>
+                                <ol>
+                                    <li>
+                                        A button to log &quot;hello world&quot;
+                                    </li>
+                                    <li>A two column layout</li>
+                                    <li>Put red rectangles in each column</li>
+                                    <div
+                                        style={{
+                                            backgroundColor: "red",
+                                            width: "50px",
+                                            height: "100px"
+                                        }}
+                                    />
+                                </ol>
+                            </Col>
+                            <Col>
+                                <p>This is the button I was talking about</p>
+                                <Button
+                                    onClick={() => console.log("Hello World!")}
+                                >
+                                    Log Hello World
+                                </Button>
+                                <div
+                                    style={{
+                                        backgroundColor: "red",
+                                        width: "225px",
+                                        height: "75px"
+                                    }}
+                                />
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
+            </div>
             <hr></hr>
             <Counter></Counter>
             <hr />
@@ -25,6 +76,14 @@ function App(): JSX.Element {
             <ChangeType></ChangeType>
             <hr />
             <CycleHoliday></CycleHoliday>
+            <hr />
+            <div>
+                <p>Here, take this cool Windows wallpaper</p>
+                <img
+                    src={require("./assets/images/LockScreen_533edc733b885d0e1188f320562b68b577c84658b6bcd3b33b729493209ae815.jpg")}
+                    alt="A Windows Wallpaper of waterside houses that looks nice. attempt 15"
+                />
+            </div>
         </div>
     );
 }
